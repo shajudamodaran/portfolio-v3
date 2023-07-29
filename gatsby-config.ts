e8b4ce62bbff2e1f53888c31ce58494a76394f75
Shaju PD <shajudamodaran7725@gmail.com>
2023-07-29 22:59:32 +0530

    GA changes

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -22,29 +22,21 @@ const config: GatsbyConfig = {
       },
     },
     {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: "G-S46VYN71L0",
+      },
+    },
+    {
       resolve: `gatsby-plugin-google-gtag`,
       options: {
-        // You can add multiple tracking ids and a pageview event will be fired for all of them.
         trackingIds: [
           "G-S46VYN71L0", // Google Analytics / GA
-
         ],
-        // This object gets passed directly to the gtag config command
-        // This config will be shared across all trackingIds
-        gtagConfig: {
-          optimize_id: "OPT_CONTAINER_ID",
-          anonymize_ip: true,
-          cookie_expires: 0,
-        },
-        // This object is used for configuration specific to this plugin
         pluginConfig: {
           // Puts tracking script in the head instead of the body
-          head: true,
-  
-          // Defaults to https://www.googletagmanager.com
-          origin: "https://www.codewithshaju.com",
-          // Delays processing pageview events on route update (in milliseconds)
-          delayOnRouteUpdate: 0,
+          head: true
         },
       },
     },
