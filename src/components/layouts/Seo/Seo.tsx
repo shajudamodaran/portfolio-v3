04aa99ba9bbbef55de5b50015ece5f2e181239ae
Shaju PD <shajudamodaran7725@gmail.com>
2023-07-30 10:05:49 +0530

    Meta data added

--- src/components/layouts/Seo/Seo.tsx
+++ src/components/layouts/Seo/Seo.tsx
@@ -19,6 +19,35 @@ function Seo({ children }: PropsType) {
             href="https://fonts.googleapis.com/css2?family=Mochiy+Pop+One&family=Open+Sans:wght@300;400;500;600;700;800&family=Poppins:wght@100;200;300;400;500;600;700;800;900&family=Roboto:wght@100;300;400;500;700;900&display=swap"
             rel="stylesheet"
           />
+
+          <meta charSet="UTF-8" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
+          <title>MERN Stack Development Services | Shaju PD</title>
+          <meta
+            name="description"
+            content="We offer MERN stack development services to create static websites and custom web applications tailored to your business needs."
+          />
+          <meta
+            name="keywords"
+            content="MERN stack, web development, static websites, custom web applications, business solutions"
+          />
+          <meta name="author" content="Shaju PD" />
+          <link rel="canonical" href="https://codewithshaju.com" />
+          <meta
+            property="og:title"
+            content="MERN Stack Development Services | Shaju PD"
+          />
+          <meta
+            property="og:description"
+            content="MERN stack development services to create static websites and custom web applications tailored to your business needs."
+          />
+          <meta
+            property="og:image"
+            content="https://ik.imagekit.io/hxburx5mi/Portfolio_assets/Capture.PNG?updatedAt=1690691491823"
+          />
         </Helmet>
         {children}
       </div>
